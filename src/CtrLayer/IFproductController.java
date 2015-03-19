01eafc9ae9b5844680754cfbaa935c15ca7ccd8c
Q11x <q11x@q11x.net>
2015-03-19 12:12:45 +0100

    Created interface for ProductController

---
+++ src/CtrLayer/IFproductController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package CtrLayer;
+
+import ModelLayer.Product;
+
+/**
+ * @author Jacob
+ *
+ */
+public interface IFproductController {
+
+	public boolean addProduct(String supplierName, String name, double PurchasePrice, double salesPrice);
+	
+	public Product findProduct(String name);
+	
+	public boolean updateProduct(String oldName, String newName, double purchasePrice, double SalesPrice, String countryOfOrigin);
+	
+	public boolean addItem(String productName, String warehouse, int amount);
+	
+	public boolean deleteProduct(String name);
+}
