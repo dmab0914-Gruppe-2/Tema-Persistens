58724b7e659a7596642a847417069ee98cbd97f5
SplintDK <splint@splintnet.dk>
2015-03-20 12:33:35 +0100

    SupplierController added
    Added supplierController with autogenerated from IFSupplierController

--- src/CtrLayer/SupplierController.java
+++ src/CtrLayer/SupplierController.java
@@ -3,10 +3,53 @@
  */
 package CtrLayer;
 
+import ModelLayer.Supplier;
+
 /**
  * @author Andreas
  *
  */
-public class SupplierController {
+public class SupplierController implements IFSupplierController {
+	
+	public SupplierController()
+	{
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see CtrLayer.IFSupplierController#addSupplier(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+	 */
+	@Override
+	public int addSupplier(String name, String address, String country, String phoneNo, String email) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see CtrLayer.IFSupplierController#findSupplier(int)
+	 */
+	@Override
+	public Supplier findSupplier(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see CtrLayer.IFSupplierController#updateSupplier(int)
+	 */
+	@Override
+	public int updateSupplier(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see CtrLayer.IFSupplierController#deleteSupplier(int)
+	 */
+	@Override
+	public int deleteSupplier(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
