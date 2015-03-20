c6c773a5725102f23f3c25bc9e0934c85070c30b
Q11x <q11x@q11x.net>
2015-03-20 11:50:50 +0100

    added implementation of ProductController interface
    there's not any actual instance of DBProduct as It's not done yet.

---
+++ src/CtrLayer/ProductController.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package CtrLayer;
+
+import DBLayer.IFDBProduct;
+import ModelLayer.Product;
+
+/**
+ * @author Jacob
+ *
+ */
+public class ProductController implements IFProductController {
+
+	IFDBProduct dbProduct;
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see CtrLayer.IFProductController#addProduct(java.lang.String,
+	 * java.lang.String, double, double)
+	 */
+	@Override
+	public boolean addProduct(String supplierName, String name, double purchasePrice, double salesPrice, String countryOfOrigin) {
+		Product p = new Product(supplierName, name, purchasePrice, salesPrice, countryOfOrigin);
+		return dbProduct.addProduct(p);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see CtrLayer.IFProductController#findProduct(java.lang.String)
+	 */
+	@Override
+	public Product findProduct(String name) {
+		return dbProduct.findProduct(name);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see CtrLayer.IFProductController#updateProduct(java.lang.String,
+	 * java.lang.String, double, double, java.lang.String)
+	 */
+	@Override
+	public boolean updateProduct(String oldName, String newName, double purchasePrice, double salesPrice, String countryOfOrigin) {
+		return dbProduct.updateProduct(oldName, newName, purchasePrice, salesPrice, countryOfOrigin);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see CtrLayer.IFProductController#addItem(java.lang.String,
+	 * java.lang.String, int)
+	 */
+	@Override
+	public boolean addItem(String productName, String warehouse, int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see CtrLayer.IFProductController#deleteProduct(java.lang.String)
+	 */
+	@Override
+	public boolean deleteProduct(String name) {
+		return dbProduct.deleteProduct(name);
+	}
+
+}
