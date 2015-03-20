637fe1431a0851f8afed65110c196c6025b63ac6
Q11x <q11x@q11x.net>
2015-03-20 12:25:57 +0100

    Revert "removed productController interface"
    This reverts commit 6eaefa0532e92ecb8e309b5eac267dc7bc5bca87.

--- src/CtrLayer/ProductController.java
+++ src/CtrLayer/ProductController.java
@@ -3,7 +3,6 @@
  */
 package CtrLayer;
 
-import DBLayer.DBProduct;
 import DBLayer.IFDBProduct;
 import ModelLayer.Product;
 
@@ -11,19 +10,17 @@ import ModelLayer.Product;
  * @author Jacob
  *
  */
-public class ProductController {
+public class ProductController implements IFProductController {
 
 	IFDBProduct dbProduct;
 	
-	/**
-	 * Create and add a new product
-	 * @param supplierName
-	 * @param name
-	 * @param purchasePrice
-	 * @param salesPrice
-	 * @param countryOfOrigin
-	 * @return
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see CtrLayer.IFProductController#addProduct(java.lang.String,
+	 * java.lang.String, double, double)
 	 */
+	@Override
 	public boolean addProduct(String supplierName, String name, double purchasePrice, double salesPrice, String countryOfOrigin) {
 		Product p = new Product(supplierName, name, purchasePrice, salesPrice, countryOfOrigin);
 		return dbProduct.addProduct(p);
@@ -34,6 +31,7 @@ public class ProductController {
 	 * 
 	 * @see CtrLayer.IFProductController#findProduct(java.lang.String)
 	 */
+	@Override
 	public Product findProduct(String name) {
 		return dbProduct.findProduct(name);
 	}
@@ -44,6 +42,7 @@ public class ProductController {
 	 * @see CtrLayer.IFProductController#updateProduct(java.lang.String,
 	 * java.lang.String, double, double, java.lang.String)
 	 */
+	@Override
 	public boolean updateProduct(String oldName, String newName, double purchasePrice, double salesPrice, String countryOfOrigin) {
 		return dbProduct.updateProduct(oldName, newName, purchasePrice, salesPrice, countryOfOrigin);
 	}
@@ -54,6 +53,7 @@ public class ProductController {
 	 * @see CtrLayer.IFProductController#addItem(java.lang.String,
 	 * java.lang.String, int)
 	 */
+	@Override
 	public boolean addItem(String productName, String warehouse, int amount) {
 		// TODO Auto-generated method stub
 		return false;
@@ -64,6 +64,7 @@ public class ProductController {
 	 * 
 	 * @see CtrLayer.IFProductController#deleteProduct(java.lang.String)
 	 */
+	@Override
 	public boolean deleteProduct(String name) {
 		return dbProduct.deleteProduct(name);
 	}
