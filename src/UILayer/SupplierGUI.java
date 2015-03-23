4e5ee44bc34df5538df688c546a4a47af280fa76
SplintDK <splint@splintnet.dk>
2015-03-23 12:44:44 +0100

    SupplierUI
    Added Search by name empty method

--- src/UILayer/SupplierGUI.java
+++ src/UILayer/SupplierGUI.java
@@ -40,6 +40,7 @@ public class SupplierGUI extends JPanel {
 	JButton btnCancel;
 	JButton btnClear;
 	JButton btnRevertChanges;
+	JButton btnFindByName;
 	private JTextField textField_id;
 	private Supplier supplier;
 	
@@ -127,22 +128,29 @@ public class SupplierGUI extends JPanel {
 		textField_id = new JTextField();
 		textField_id.setEditable(false);
 		textField_id.setColumns(10);
+		
+		btnFindByName = new JButton("Find by Name");
+
 		GroupLayout gl_layeredPane = new GroupLayout(layeredPane);
 		gl_layeredPane.setHorizontalGroup(
 			gl_layeredPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_layeredPane.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_layeredPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblSupplier)
 						.addGroup(gl_layeredPane.createSequentialGroup()
-							.addComponent(textField_idInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnFindById))
-						.addGroup(gl_layeredPane.createParallelGroup(Alignment.TRAILING, false)
-							.addComponent(btnNewSupplier, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(btnClear, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(btnDeleteSupplier, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-					.addGap(27)
+							.addGroup(gl_layeredPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblSupplier)
+								.addGroup(gl_layeredPane.createParallelGroup(Alignment.TRAILING, false)
+									.addComponent(btnNewSupplier, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(btnClear, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(btnDeleteSupplier, Alignment.LEADING))
+								.addComponent(textField_idInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGap(6))
+						.addGroup(Alignment.TRAILING, gl_layeredPane.createSequentialGroup()
+							.addGroup(gl_layeredPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(btnFindById, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 105, Short.MAX_VALUE)
+								.addComponent(btnFindByName, GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED)))
 					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_layeredPane.createParallelGroup(Alignment.LEADING)
@@ -160,21 +168,21 @@ public class SupplierGUI extends JPanel {
 										.addComponent(lblEmail))
 									.addGap(18)
 									.addGroup(gl_layeredPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(textField_phone, GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE)
-										.addComponent(textField_email, GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE)
+										.addComponent(textField_phone, GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE)
+										.addComponent(textField_email, GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE)
 										.addComponent(textField_id, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
 								.addGroup(gl_layeredPane.createSequentialGroup()
 									.addComponent(lblNewLabel)
 									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(textField_country, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE))
+									.addComponent(textField_country, GroupLayout.DEFAULT_SIZE, 289, Short.MAX_VALUE))
 								.addGroup(gl_layeredPane.createSequentialGroup()
 									.addGroup(gl_layeredPane.createParallelGroup(Alignment.LEADING)
 										.addComponent(lblAddress)
 										.addComponent(lblName))
 									.addPreferredGap(ComponentPlacement.UNRELATED)
 									.addGroup(gl_layeredPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(textField_name, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
-										.addComponent(textField_address, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE))))
+										.addComponent(textField_name, GroupLayout.DEFAULT_SIZE, 289, Short.MAX_VALUE)
+										.addComponent(textField_address, GroupLayout.DEFAULT_SIZE, 289, Short.MAX_VALUE))))
 							.addGap(249))
 						.addComponent(lblNewLabel_1))
 					.addContainerGap())
@@ -190,10 +198,12 @@ public class SupplierGUI extends JPanel {
 								.addGroup(gl_layeredPane.createSequentialGroup()
 									.addComponent(lblSupplier)
 									.addGap(18)
-									.addGroup(gl_layeredPane.createParallelGroup(Alignment.BASELINE)
-										.addComponent(textField_idInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addComponent(btnFindById))
-									.addGap(30)
+									.addComponent(textField_idInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(btnFindById)
+									.addGap(18)
+									.addComponent(btnFindByName)
+									.addGap(47)
 									.addComponent(btnNewSupplier)
 									.addGap(18)
 									.addComponent(btnClear)
@@ -223,7 +233,7 @@ public class SupplierGUI extends JPanel {
 									.addGroup(gl_layeredPane.createParallelGroup(Alignment.BASELINE)
 										.addComponent(lblNewLabel_1)
 										.addComponent(textField_id, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
-							.addPreferredGap(ComponentPlacement.RELATED, 229, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED, 148, Short.MAX_VALUE)
 							.addGroup(gl_layeredPane.createParallelGroup(Alignment.BASELINE)
 								.addComponent(btnSave)
 								.addComponent(btnCancel)
@@ -235,7 +245,7 @@ public class SupplierGUI extends JPanel {
 		actionListerners();
 	}
 	
-	public void findSupplier()
+	public void findSupplierID()
 	{
 		
 		int i = -1;
@@ -310,7 +320,7 @@ public class SupplierGUI extends JPanel {
 		
 		btnFindById.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				findSupplier();
+				findSupplierID();
 			}
 		});
 		
@@ -329,6 +339,11 @@ public class SupplierGUI extends JPanel {
 		btnRevertChanges.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				revertChanges();
+			}
+		});
+		
+		btnFindByName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 			}
 		});
 	}
